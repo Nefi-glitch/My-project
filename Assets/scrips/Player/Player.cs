511a870a19cb3ce08edf969b0a94385695c11587
Nefi-glitch <navarronefi55@gmail.com>
2025-04-29 23:41:35 -0600

    inputsystems

--- Assets/scrips/Player/Player.cs
+++ Assets/scrips/Player/Player.cs
@@ -6,26 +6,12 @@ public class Player : MonoBehaviour
 {
 
    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput; 
 
     private bool iswalking;
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        { inputVector.y = +1; }
-
-        if (Input.GetKey(KeyCode.S))
-        { inputVector.y = -1; }
-
-        if (Input.GetKey(KeyCode.A))
-        { inputVector.x = -1; }
-
-        if (Input.GetKey(KeyCode.D))
-        { inputVector.x = +1; }
-
-        inputVector = inputVector.normalized;
-
-        
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Debug.Log(inputVector);
 
