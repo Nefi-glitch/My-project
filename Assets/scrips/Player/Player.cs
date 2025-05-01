31f9f100314aa6ea51df804f29fdfbd633586e30
Nefi-glitch <navarronefi55@gmail.com>
2025-04-30 21:05:12 -0600

    collider

--- Assets/scrips/Player/Player.cs
+++ Assets/scrips/Player/Player.cs
@@ -13,13 +13,43 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
-        Debug.Log(inputVector);
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+       
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerHeight = 2f; 
+        float playerRadius = .7f;
+        bool canMove = !Physics.CapsuleCast(transform.position,transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
 
-        iswalking = moveDir != Vector3.zero;
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else { }
+
+                if (canMove)
+                {
+                    transform.position += moveDir * moveDistance;
+                    iswalking = moveDir != Vector3.zero;
+                }
+            }
+        }
         float rotateSpeed = 5f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime *rotateSpeed);   
 
