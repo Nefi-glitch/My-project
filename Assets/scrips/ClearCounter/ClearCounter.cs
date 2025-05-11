625d98dce1b5714a106cd6ddafcf6e9f78beb20e
Nefi-glitch <navarronefi55@gmail.com>
2025-05-10 23:27:05 -0600

    KitchenObjectSO

--- Assets/scrips/ClearCounter/ClearCounter.cs
+++ Assets/scrips/ClearCounter/ClearCounter.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectOS kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
     public void Interact()
     {
         Debug.Log("Interact");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        Debug.Log(KitchenObjectTransform.GetComponent <KitchenObjectTransform> ().GetKitchenObjectSO().obejctName);
     }
 }
